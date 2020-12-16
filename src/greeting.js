5a4011f7c6a8afbc00d494f317dcbabee3aae9f3
zhouxing <zhouxing@reconova.cn>
2020-12-16 23:48:43 +0800

    实现一个简单的webpack 首次提交

---
+++ src/greeting.js
@@ -0,0 +1,3 @@
+export function greeting (name){
+    return "My name is " + name + " hello ";
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {greeting} from "./greeting.js";
+
+console.log(greeting('hay'));
+
+document.write(greeting('hay'))
